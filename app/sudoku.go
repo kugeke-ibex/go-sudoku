4bde63d4a89b5000f1b9a3d52a8260c06bd7d578
Kenjiro Kuge <kugetyan0211@gmail.com>
2023-12-30 12:06:02 +0000

    add function pretty

--- app/sudoku.go
+++ app/sudoku.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+)
+
+// Boardはsudokuのboardを示す
+
+// 0: 未入力
+// 1-9: 入力
+type Board [9][9]int
+func pretty (b Board) string {
+	var buf bytes.Buffer
+	for i := 0; i < 9; i++ {
+		if i%3 == 0 {
+				buf.WriteString("+---+---+---+\n")
+		}
+		for j := 0; j < 9; j++ {
+			if j%3 == 0 {
+				buf.WriteString("|")
+			}
+			buf.WriteString(strconv.Itoa(b[i][j]))
+		}
+		buf.WriteString("|\n")
+	}
+	buf.WriteString("+---+---+---+\n")
+	return buf.String()
+}
+
 func main () {
-	
+	b := Board{
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	fmt.Printf("%v", pretty(b))
 }
