e3ce060cd0390740ffa03e6cf366279dd0bf149a
Kenjiro Kuge <kugetyan0211@gmail.com>
2023-12-30 13:08:28 +0000

    create test code

--- app/sudoku.go
+++ app/sudoku.go
@@ -11,11 +11,12 @@ import (
 // 0: 未入力
 // 1-9: 入力
 type Board [9][9]int
-func pretty (b Board) string {
+
+func pretty(b Board) string {
 	var buf bytes.Buffer
 	for i := 0; i < 9; i++ {
 		if i%3 == 0 {
-				buf.WriteString("+---+---+---+\n")
+			buf.WriteString("+---+---+---+\n")
 		}
 		for j := 0; j < 9; j++ {
 			if j%3 == 0 {
@@ -29,7 +30,62 @@ func pretty (b Board) string {
 	return buf.String()
 }
 
-func main () {
+func duplicated(c [10]int) bool {
+	for k, v := range c {
+		if k == 0 {
+			continue
+		}
+		if v >= 2 {
+			return true
+		}
+	}
+	return false
+}
+
+func verify(b Board) bool {
+	// 行チェック
+	for i := 0; i < 9; i++ {
+		// 出現回数
+		var c [10]int
+		for j := 0; j < 9; j++ {
+			c[b[i][j]]++
+		}
+		if duplicated(c) {
+			return false
+		}
+	}
+
+	// 列チェック
+	for i := 0; i < 9; i++ {
+		// 出現回数
+		var c [10]int
+		for j := 0; j < 9; j++ {
+			c[b[j][i]]++
+		}
+		if duplicated(c) {
+			return false
+		}
+	}
+
+	// 3×3のチェック
+	for i := 0; i < 9; i += 3 {
+		for j := 0; j < 9; j += 3 {
+			var c [10]int
+			for row := i; row < i+3; row++ {
+				for col := j; col < j+3; col++ {
+					c[b[row][col]]++
+				}
+			}
+			if duplicated(c) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func main() {
 	b := Board{
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
